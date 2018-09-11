8e0b50c2e5116eb68869a61038441a0d88f1eaf8
RC0030800 <RC0030800@CRO-24-15>
2018-09-11 10:56:17 +0100

    added comments

--- MovePicture/Form1.cs
+++ MovePicture/Form1.cs
@@ -41,27 +41,32 @@ namespace MovePicture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
         }
-
+        //moves diagonal downwards to the left
         private void button5_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 5);
         }
-
+        //moves downwards diagonal to the right
         private void button6_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 5);
 
                 
         }
-
+        //moves upwards diagonal to the left
         private void button7_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y - 5);
         }
-
+        //moves diagonal upwards to the right
         private void button8_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y - 5);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
